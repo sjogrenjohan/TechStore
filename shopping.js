550867664cd3eb116e344ba25623493b1ca22efe
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2018-11-06 09:38:32 +0100

    function

--- shopping.js
+++ shopping.js
@@ -6,14 +6,3 @@ function initSite() {
 
 
 
-
-function clickME() {
-    if(typeof(Storage) !== "undefined") {
-        if (localStorage.clickcount) {
-            localStorage.clickcount = Number(localStorage.clickcount)+1;
-        } else {
-            localStorage.clickcount = 1;
-        }
-        document.getElementById("clicks").innerHTML = " " + localStorage.clickcount + "";
-    } 
-}

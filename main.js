412fa13472bb03b5b5c1fb406a7c0f82ac9e2b64
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2018-11-06 08:56:56 +0100

    Fixed localStorage click

--- main.js
+++ main.js
@@ -62,17 +62,20 @@ function createMobileCard(listOfProducts) {
     mobileProduct.appendChild(shoppingButton)
     shoppingButton.appendChild(shoppingCart)
     shoppingButton.appendChild(shoppingCartText)
-    
-
-
 
     return mobileProduct;
 }
 
 
-var clicks = 0;
 function clickME() {
-    clicks += 1;
-    document.getElementById("clicks").innerHTML = clicks;
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.clickcount) {
+            localStorage.clickcount = Number(localStorage.clickcount)+1;
+        } else {
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("clicks").innerHTML = " " + localStorage.clickcount + "";
+    } else {
+        document.getElementById("clicks").innerHTML = "Sorry, your browser does not support web storage...";
+    }
 }
-

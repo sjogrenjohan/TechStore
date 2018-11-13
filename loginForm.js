076e4e2dbd78becc401b22fa6cc336a64d98943c
sjogrenjohan <sjogren_johan@hotmail.com>
2018-11-13 11:01:31 +0100

    fixed registration and log in

--- loginForm.js
+++ loginForm.js
@@ -1,28 +1,52 @@
-// Name and Password from the register-form
-var name = document.getElementById('exampleInputEmail1');
-var pw = document.getElementById('exampleInputPassword1');
+var shoppingProducts = JSON.parse(localStorage.productArray);
+
+function remember() {
+    document.getElementById("clicks").innerHTML = " " + shoppingProducts.length + "";
+}
+
+
+
+
 
 // storing input from register-form
 function store() {
-    localStorage.setItem('exampleInputEmail1', name.value);
-    localStorage.setItem('exampleInputPassword1', pw.value);
+    // Name and Password from the register-form
+    var name = document.getElementById('exampleInputEmail1');
+    var pw = document.getElementById('exampleInputPassword1');
+    var newUser =  {
+        userName: name.value,
+        userPassword: pw.value
+    }
+    var userAccounts = JSON.parse(localStorage.getItem("userAccounts")) 
+    if(!userAccounts) {
+        userAccounts = [newUser]
+    }
+    else {
+        userAccounts.push(newUser)
+    }
+    console.log(userAccounts)
+    
+    localStorage.setItem("userAccounts", JSON.stringify(userAccounts))
+
 }
 
 // check if stored data from register-form is equal to entered data in the   login-form
 function check() {
 
-    // stored data from the register-form
-    var storedName = localStorage.getItem('exampleInputEmail1');
-    var storedPw = localStorage.getItem('exampleInputPassword1');
+    var userAccounts = JSON.parse(localStorage.getItem("userAccounts"));
 
     // entered data from the login-form
-    var userName = document.getElementById('userName');
-    var userPw = document.getElementById('userPassword');
+    var inputName = document.getElementById('userName');
+    var inputPassword = document.getElementById('userPassword');
 
     // check if stored data from register-form is equal to data from login form
-    if(userName.value == storedName && userPw.value == storedPw) {
-        alert('You are logged in.');
-    }else {
-        alert('ERROR.');
+    for(var i = 0; i < userAccounts.length; i++) {
+        console.log(inputName, inputPassword)
+        if(userAccounts[i].userName == inputName.value && userAccounts[i].userPassword == inputPassword.value) {
+            window.location.href = "userLoggedIn.html"
+            alert('You are logged in.');
+            break
+        }
     }
+    
 }
